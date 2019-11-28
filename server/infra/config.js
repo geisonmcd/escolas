ef8c6f19bc62ff90055a955996aab377df950c57
Geison Machado <geisonmcd@gmail.com>
2019-11-27 23:12:37 -0300

    Deixando valores da config do banco de dados constante

--- server/infra/config.js
+++ server/infra/config.js
@@ -6,7 +6,7 @@ config.database.db = 'institutions';
 config.database.pw = '123456';
 config.database.host = 'localhost';
 config.database.port = 5432;
-config.database.maxConnections = process.env.PERSONS_POOL_MAX || 10;
-config.database.idleTimeoutMillis = process.env.PERSONS_POOL_TIMEOUT || 30000;
+config.database.maxConnections =  10;
+config.database.idleTimeoutMillis = 30000;
 
 module.exports = config;
