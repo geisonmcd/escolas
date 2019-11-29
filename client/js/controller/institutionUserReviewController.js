47c9f83ae30ac1c7ae46b9e7b83119e97dcfefed
Geison Machado <geisonmcd@gmail.com>
2019-11-28 21:53:30 -0300

    Mostrando mensagem de sucesso ao salvar modal

--- client/js/controller/institutionUserReviewController.js
+++ client/js/controller/institutionUserReviewController.js
@@ -7,12 +7,21 @@ angular.module("institutions").controller("institutionUserReviewController", ["$
     $scope.saveReview = function (review) {
         if (review.idReview) {
             $http.put(`institutions/${$scope.review.idInstitution}/users/${1}/reviews/${review.idReview}`, review).then(function (response) {
+                saveSuccess();
             }); 
         } else {
             $http.post(`institutions/${$route.current.params.idInstitution}/users/${1}/reviews/`, review).then(function (response) {
+                saveSuccess();
             });
         }
     };  
+
+    const saveSuccess = function () {
+        $scope.saved = true;
+        setTimeout(function () {
+            $route.reload();
+        }, 2000)
+    }
     
     init();
 }]);
